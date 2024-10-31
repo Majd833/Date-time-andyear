12824a06fe743f68c0355f28076449b0a4eaa911
Majd833 <lina.azzam.1611@gmail.com>
2024-10-31 17:34:07 +0200

    Date and time

---
+++ Date and Time.py
@@ -0,0 +1,6 @@
+from datetime import date, time, datetime
+today=date.today()
+now=datetime.now()
+print("today is:",today)
+print("now it's:",now)
+print("today's full date is:",today.day,today.month,today.year)
